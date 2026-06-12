931e2576e940c5be8e1ac41fcb1e1f2e1333599f
shima-shima <19295364+shima-shima@users.noreply.github.com>
2026-06-12 18:01:16 +0900

    feat(nvim): IME 自動 OFF

--- files/.config/nvim/lua/autocmds.lua
+++ files/.config/nvim/lua/autocmds.lua
@@ -1,4 +1,4 @@
--- カーソル下のシンボルハイライト設定
+-- LSP 有効時にカーソル下のシンボルハイライト設定
 vim.api.nvim_create_augroup('LspDocumentHighlight', { clear = true })
 vim.api.nvim_create_autocmd('CursorHold', {
   group = 'LspDocumentHighlight',
@@ -17,3 +17,13 @@ vim.api.nvim_create_autocmd('CursorMoved', {
   callback = vim.lsp.buf.clear_references,
 })
 vim.opt.updatetime = 500
+
+-- インサートモードを抜けた（InsertLeave）タイミングで IME をオフにする
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  callback = function()
+    -- OSに応じたデフォルトの英字入力を指定（Mac: com.apple.keylayout.ABC, Windows: 1033 など）
+    -- ここでは例として Mac の標準英字レイアウトを指定
+    vim.fn.system({ "im-select", "com.apple.keylayout.ABC" })
+  end,
+})
